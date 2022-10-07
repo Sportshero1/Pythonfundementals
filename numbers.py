9fa28a30c1d32d6260c8e21bf15ffa088b9852e6
Sportshero1 <113217313+Sportshero1@users.noreply.github.com>
2022-10-06 18:10:41 -0600

    Init Repo

---
+++ numbers.py
@@ -0,0 +1,18 @@
+import random
+
+myInteger = 1923
+secondInteger = 1978
+myRandomYear = random.randint(1974,2022)
+print(myRandomYear)
+
+timepassed = (myRandomYear - 1923)
+print(timepassed)
+
+appleprice = 1.79
+orangeprice = 1.25
+bananaprice = 2
+bananapricefloat = float(bananaprice)
+
+bill = (appleprice + orangeprice + bananaprice)
+
+print(bill)
